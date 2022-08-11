20a802f0a9b099d0108177c85762756bcad95255
Javier Vargas <3231javiervargas@gmail.com>
2022-08-11 14:14:26 -0400

    fix bug

--- src/App.js
+++ src/App.js
@@ -1,21 +1,84 @@
 import { useState } from 'react';
-import Login from './componets/Login';
-import SecretStuff from './componets/SecretStuff';
-import './App.css';
+import { initializeApp } from 'firebase/app';
+import { 
+  createUserWithEmailAndPassword, getAuth,
+  GoogleAuthProvider,
+  signInWithEmailAndPassword, 
+  signInWithPopup} from 'firebase/auth';
 
-function App() {
-  const [isLoggedIn, setIsLoggedIn] = useState()
+const firebaseConfig = {
+  apiKey: "AIzaSyDWjvPr030P8uoXip7_Wbraa4cgxQ7BcVo",
+  authDomain: "first-login-bc.firebaseapp.com",
+  projectId: "first-login-bc",
+  storageBucket: "first-login-bc.appspot.com",
+  messagingSenderId: "161161692397",
+  appId: "1:161161692397:web:382a9c9736bc15f6e138e4"
+};
+
+function Login({ setIsLoggedIn }) {
+  const [email, setEmail] = useState('')
+  const [password, setPassword] = useState('')
+
+  const connectAuth = async () => {
+    // connect to firebase project
+    const app = initializeApp(firebaseConfig);
+    // connect to Auth
+    return getAuth(app);
+  }
+
+  const handleLogin = async () => {
+    const auth = await connectAuth()
+    const user = await signInWithEmailAndPassword(auth, email, password)
+      .catch(err => alert(err.message))
+    if(user) {
+      console.log(user.user)
+      setIsLoggedIn(true)
+    }
+  }
+
+  const handleGoogleLogin = async () => {
+    const auth = await connectAuth()
+    const provider = new GoogleAuthProvider()
+    const user = await signInWithPopup(auth, provider)
+      .catch(err => alert(err.message))
+    if(user) {
+      console.log(user.user)
+      setIsLoggedIn(true)
+    }
+  }
+
+  const handleSignUp = async () => {
+    const auth = await connectAuth()
+    // send email and password to firebase auth
+    const user = await createUserWithEmailAndPassword(auth, email, password)
+      .catch(err => alert(err.message))
+    // if all ok...
+    if(user) {
+      console.log(user.user)
+      setIsLoggedIn(true)
+    }
+  }
 
   return (
-    <>
-    <header>
-      <h1>My first login</h1>
-    </header>
-    {
-      isLoggedIn? <SecretStuff/>: <Login setIsLoggedIn={setIsLoggedIn}/>
-    }
-    </>
-  );
+    <form onSubmit={(e) => e.preventDefault()}>
+      <label htmlFor="email">
+        Email:
+        <input
+          value={email} onChange={e => setEmail(e.target.value)}
+          name="email" type="email" placeholder="you@there.com" />
+      </label><br />
+      <label htmlFor="password">
+        Password:
+        <input
+          value={password} onChange={e => setPassword(e.target.value)}
+          name="password" type="password" />
+      </label><br />
+      <button onClick={handleLogin}>Login</button>&nbsp;
+      <button onClick={handleSignUp}>Sign Up</button>
+      <br />
+      <button onClick={handleGoogleLogin}>Login with Google</button>
+    </form>
+  )
 }
 
-export default App;
+export default Login;
--- src/componets/Login.jsx
+++ src/componets/Login.jsx
@@ -1,99 +1,84 @@
-import { useState } from "react"
-import { initializeApp } from "firebase/app"
-import {
-  createUserWithEmailAndPassword,
-  getAuth,
+import { useState } from 'react';
+import { initializeApp } from 'firebase/app';
+import { 
+  createUserWithEmailAndPassword, getAuth,
   GoogleAuthProvider,
-  signInWithEmailAndPassword,
-  signInWithPopup,
-} from "firebase/auth"
+  signInWithEmailAndPassword, 
+  signInWithPopup} from 'firebase/auth';
 
 const firebaseConfig = {
-  apiKey: "AIzaSyCv1KE5oGJtLYOHCDfxMgHMXChJ8aL7kME",
-  authDomain: "first-login-jv.firebaseapp.com",
-  projectId: "first-login-jv",
-  storageBucket: "first-login-jv.appspot.com",
-  messagingSenderId: "483788916464",
-  appId: "1:483788916464:web:a6c61e22d79f380b35a435",
-}
+  apiKey: "AIzaSyDWjvPr030P8uoXip7_Wbraa4cgxQ7BcVo",
+  authDomain: "first-login-bc.firebaseapp.com",
+  projectId: "first-login-bc",
+  storageBucket: "first-login-bc.appspot.com",
+  messagingSenderId: "161161692397",
+  appId: "1:161161692397:web:382a9c9736bc15f6e138e4"
+};
 
-export default function Login({ setIsLoggedIn }) {
-  const [email, setEmail] = useState("")
-  const [password, setPassword] = useState("")
+function Login({ setIsLoggedIn }) {
+  const [email, setEmail] = useState('')
+  const [password, setPassword] = useState('')
 
-  const connectAuth = () => {
-    // connect to our firebase project
-    const app = initializeApp(firebaseConfig)
+  const connectAuth = async () => {
+    // connect to firebase project
+    const app = initializeApp(firebaseConfig);
     // connect to Auth
-    return getAuth(app)
+    return getAuth(app);
   }
 
-  const handlelogin = async () => {
-    const auth = connectAuth()
-    const user = await signInWithEmailAndPassword(auth, email, password).catch(
-      (err) => alert(err.message)
-    )
-    if (user) {
+  const handleLogin = async () => {
+    const auth = await connectAuth()
+    const user = await signInWithEmailAndPassword(auth, email, password)
+      .catch(err => alert(err.message))
+    if(user) {
       console.log(user.user)
       setIsLoggedIn(true)
     }
   }
 
-  const handleGoogleLogin = async ()=>{
+  const handleGoogleLogin = async () => {
     const auth = await connectAuth()
     const provider = new GoogleAuthProvider()
     const user = await signInWithPopup(auth, provider)
-    .catch(err => alert(err.message))
-    if(user){
-        console.log(user.user)
-        setIsLoggedIn(true)
+      .catch(err => alert(err.message))
+    if(user) {
+      console.log(user.user)
+      setIsLoggedIn(true)
     }
   }
 
   const handleSignUp = async () => {
-    const auth = connectAuth()
-    //send email & password to firebase auth
-    const user = await createUserWithEmailAndPassword(
-      auth,
-      email,
-      password
-    ).catch((err) => alert(err.message))
-    // if all ok then set
-    if (user) {
-      console.log(user)
+    const auth = await connectAuth()
+    // send email and password to firebase auth
+    const user = await createUserWithEmailAndPassword(auth, email, password)
+      .catch(err => alert(err.message))
+    // if all ok...
+    if(user) {
+      console.log(user.user)
       setIsLoggedIn(true)
     }
-
-    // if error
-    //popup error
   }
+
   return (
     <form onSubmit={(e) => e.preventDefault()}>
       <label htmlFor="email">
         Email:
         <input
-          value={email}
-          onChange={(e) => setEmail(e.target.value)}
-          type="email"
-          name="email"
-          placeholder="you@there.com"
-        />
-      </label>
-      <br />
+          value={email} onChange={e => setEmail(e.target.value)}
+          name="email" type="email" placeholder="you@there.com" />
+      </label><br />
       <label htmlFor="password">
         Password:
         <input
-          value={password}
-          onChange={(e) => setPassword(e.target.value)}
-          type="password"
-          name="password"
-        />
-      </label>
-      <br />
-      <button onClick={handlelogin}>Login</button>&nbsp;
+          value={password} onChange={e => setPassword(e.target.value)}
+          name="password" type="password" />
+      </label><br />
+      <button onClick={handleLogin}>Login</button>&nbsp;
       <button onClick={handleSignUp}>Sign Up</button>
       <br />
       <button onClick={handleGoogleLogin}>Login with Google</button>
     </form>
   )
 }
+
+export default Login;
